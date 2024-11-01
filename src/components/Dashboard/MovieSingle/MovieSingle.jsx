4504588640f31c2b7f79b8784d691476e095644a
Yosber Guerrero <yosbergc@gmail.com>
2024-11-01 14:26:20 -0400

    Added moviesingle component, added styles to moviesingle

---
+++ src/components/Dashboard/MovieSingle/MovieSingle.jsx
@@ -0,0 +1,35 @@
+import './moviesingle.css'
+import { useState } from 'react'
+import { FaPlay } from "react-icons/fa";
+import { GrLike } from "react-icons/gr";
+import { GrDislike } from "react-icons/gr";
+import { FaRegHeart } from "react-icons/fa";
+function MovieSingle({ titulo, descripcion, duracion }) {
+    const [showMovieInfo, setShowMovieInfo] = useState(false)
+    return (
+        <section className="movie-single-container">
+            <section className="movie-show" onMouseEnter={() => setShowMovieInfo(true)}>
+                <img src="https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2023/11/08/imagen-promocional-de-jujutsu-kaisen.jpeg" alt="Jujutsu Kaisen" />
+            </section>
+            {
+                showMovieInfo && <section className="movie-inner" onMouseLeave={() => setShowMovieInfo(false)}>
+                    <img src="https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2023/11/08/imagen-promocional-de-jujutsu-kaisen.jpeg" alt="Jujutsu Kaisen" />
+                    <section className="movie-inner-content">
+                        <h3>Jujutsu Kaisen</h3>
+                        <p>Un estudiante de secundaria común y corriente con una fuerza sobrenatural impresionante. Después de un encuentro con maledictos, seres malvados que se alimentan de energía negativa, Yuji se ve involucrado en el mundo de la maldición.</p>
+
+                        <div className="buttons">
+                            <div className="single-icon">
+                                <FaPlay className='icon-movie-inner play' size={22}/>
+                            </div>
+                            <GrLike className='icon-movie-inner like' size={22}/>
+                            <GrDislike className='icon-movie-inner dislike' size={22}/>
+                            <FaRegHeart className='icon-movie-inner heart' size={22}/>
+                        </div>
+                    </section>
+                </section>
+            }
+        </section>
+    )
+}
+export { MovieSingle }
--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,9 +1,21 @@
 import { Header } from "../../components/Dashboard/Header/Header"
 import { Footer } from "../../components/Landing/Footer/Footer"
+import { MovieSingle } from "../../components/Dashboard/MovieSingle/MovieSingle"
+import './dashboard.css'
 function Dashboard() {
     return (
         <>
             <Header />
+            <section className="movies-container">
+                <h2>Anime</h2>
+                <section className="inner-movies">
+                    <MovieSingle />
+                    <MovieSingle />
+                    <MovieSingle />
+                    <MovieSingle />
+                    <MovieSingle />
+                </section>
+            </section>
             <Footer />
         </>
     )
