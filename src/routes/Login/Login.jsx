90a0f2d696a0f8829a403978a380069c56d65de4
Yosber Guerrero <yosbergc@gmail.com>
2024-10-31 23:26:10 -0400

    Added login, added styles to faq, changed font

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,8 +1,20 @@
+import './login.css'
+import { Link } from 'react-router-dom'
 function Login() {
     return (
-        <>
-        
-        </>
+        <section className="login-page">
+            <form className="login-page-form">
+                <h2>Iniciar sesión</h2>
+                <input type="text" name="email" id="email" placeholder='Email o número de celular'/>
+                <input type="password" name="email" id="password" placeholder='Contraseña' />
+
+                <button className="primary-btn">Iniciar sesión</button>
+                <div className="bottom-form">
+                    <p>¿Primera vez en Macaflix?</p>
+                    <Link to='/register'>Registrate ahora</Link>
+                </div>
+            </form>
+        </section>
     )
 }
 
