5db79e43673a95ae0dd9459db272e982eff55ef1
Yosber Guerrero <yosbergc@gmail.com>
2024-10-31 23:59:57 -0400

    Added sonner to notifications in the dashboard

--- src/routes/Landing/Landing.jsx
+++ src/routes/Landing/Landing.jsx
@@ -2,7 +2,6 @@ import { Header } from '../../components/Landing/Header/Header'
 import { WhyJoin } from '../../components/Landing/WhyJoin/WhyJoin'
 import { Footer } from '../../components/Landing/Footer/Footer'
 import { Faq } from '../../components/Landing/Faq/Faq'
-
 function Landing() {
     return (
         <>
