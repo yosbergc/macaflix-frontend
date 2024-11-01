bcbee758f499b33a9bb33a4d1487a213adfb7e33
Yosber Guerrero <yosbergc@gmail.com>
2024-11-01 15:01:48 -0400

    Added background with gradient, deleted before to hero-section background

--- src/components/Dashboard/UserDropdown/UserDropdown.jsx
+++ src/components/Dashboard/UserDropdown/UserDropdown.jsx
@@ -1,11 +1,35 @@
 import ProfilePic from '../../../assets/profile-pic.png'
 import './userdropdown.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { MdOutlineHelpOutline } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
+import { Toaster, toast } from 'sonner';
 function UserDropdown() {
+    const [showUserDropdown,  setShowUserDropdown] = useState(false)
+    function handleShow() {
+        setShowUserDropdown(!showUserDropdown)
+    }
+    function handleHelp() {
+        toast.error('Actualmente nuestro servicio de ayuda se encuentra fuera de servicio. Nos contactaremos contigo lo antes posible.')
+        setShowUserDropdown(false)
+    }
     return (
         <section className="user-dropdown-container">
-            <section className="visible-part">
+            <section className="visible-part" onClick={handleShow}>
                 <img src={ProfilePic} alt="Profile Pic" />
             </section>
+            {
+                showUserDropdown && <section className="dropdown-part">
+                    <section className="inner-dropdown">
+                        <button className="dropdown-item" onClick={handleHelp}><MdOutlineHelpOutline size={20} />Centro de ayuda</button>
+                        <Link to="/account" className="dropdown-item"><FaRegUser size={20}/> Mi cuenta</Link>
+                        <button className="dropdown-item"><FiLogOut size={20}/>Cerrar sesión en Macaflix</button>
+                    </section>
+                </section>
+            }
+            <Toaster richColors theme='dark'/>
         </section>
     )
 }
