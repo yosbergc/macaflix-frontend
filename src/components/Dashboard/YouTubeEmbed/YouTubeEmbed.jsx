f88d822e4d3031d4a316fcbb3964a04e88bf0172
Yosber Guerrero <yosbergc@gmail.com>
2024-11-02 10:59:09 -0400

    Some changes added

--- src/components/Dashboard/YouTubeEmbed/YouTubeEmbed.jsx
+++ src/components/Dashboard/YouTubeEmbed/YouTubeEmbed.jsx
@@ -5,7 +5,7 @@ function YouTubeEmbed({ embedId }) {
     }
     return (
         <div className="video-responsive">
-            <iframe width="100%" height="315" src={`http://www.youtube.com/embed/${embedId}?autoplay=1&controls=1&modestbranding=1`} title="YouTube video player" frameBorder="0" allow="autoplay" referrerPolicy="strict-origin-when-cross-origin" onMouseMove={handleMove}></iframe>
+            <iframe width="100%" height="315" src={`https://www.youtube.com/embed/${embedId}?autoplay=1&controls=1&modestbranding=1`} title="YouTube video player" frameBorder="0" allow="autoplay" referrerPolicy="strict-origin-when-cross-origin" onMouseMove={handleMove}></iframe>
         </div>
     )
 }
