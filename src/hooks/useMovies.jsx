127359e1f9cf8b1efe833608977843020507c1c7
Yosber Guerrero <yosbergc@gmail.com>
2024-11-01 23:12:50 -0400

    Added validation to prevent fetch when user is not logged

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -6,9 +6,11 @@ function useMovies() {
     const [movies, setMovies] = useState([])
     const { user } = useContext(userContext)
     useEffect(() => {
-        getMovies(user.token)
-        .then(movies => setMovies(movies))
-        .catch(error => console.error(error))
+        if (user) {
+            getMovies(user.token)
+            .then(movies => setMovies(movies))
+            .catch(error => console.error(error))
+        }
     }, [])
     return { movies }
 }
